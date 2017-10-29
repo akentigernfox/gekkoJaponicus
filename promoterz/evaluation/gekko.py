d7b051eca93c6be632bcdff06aaffd3edd583847
MAC_WINPRO\Andrew <andrew@akfdev.com>
2017-10-29 07:53:40 +0700

    no message

--- promoterz/evaluation/gekko.py
+++ promoterz/evaluation/gekko.py
@@ -30,9 +30,9 @@ def httpPost(URL, data={}):
         print("")
         print("URL          :" + URL)
         print("")
-        print("Post Data    : " + data)
-        print("")
         print("Request.text :" + Request.text)
+        print("")
+        print json.dumps(data, indent=4, sort_keys=True)
         raise e
 
     return Response
